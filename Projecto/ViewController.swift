2d3eb69f23e8ff5592f92016d8e6b87bf92d0af2
Ariadna Thau <ariadna.thau@gmail.com>
2020-07-29 11:33:29 -0700

    HELLO MY CHILD

---
+++ Projecto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projecto
+//
+//  Created by Ariadna Thau on 7/29/20.
+//  Copyright © 2020 Ariadna Thau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
